ec33ebc820fb05c6d26f813a732888e4561e065a
jadedragonette <jadedragonette@ymail.com>
2024-12-24 11:15:08 -0800

    added text_node to html_node, needs testing - started split_nodes_delimiter

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
 from textnode import *
 
+def text_node_to_html_node(text_node):
+    if text_node.TextType == TEXT:
+        LeafNode(None, text_node.text)
+    if text_node.TextType == BOLD:
+        LeafNode("b", text_node.text)
+    if text_node.TextType == ITALIC:
+        LeafNode("i", text_node.text)
+    if text_node.TextType == CODE:
+        LeafNode("code", text_node.text)
+    if text_node.TextType == LINK:
+        LeafNode("a", text_node.text, , {href:text_node.url})
+    if text_node.TextType == IMAGE:
+        LeafNode("img", "", , {src:text_node.url, alt:text_node.text})
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    return
+
 def main():
     Test = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(Test.__repr__())
