13bede14396175fc9edf155e0a552f7425ff294d
jadedragonette <jadedragonette@ymail.com>
2024-12-25 10:09:16 -0800

    framework for testing text to html added

---
+++ src/test_text_to_html.py
@@ -0,0 +1,13 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        return
+
+
+
+if __name__ == "__main__":
+    unittest.main()
