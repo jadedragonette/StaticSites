6b8f93d90a14ee1b59c6e63a94c02d7d2a34cd1a
jadedragonette <jadedragonette@ymail.com>
2024-12-31 08:32:18 -0800

    minor comment adding

--- src/test_text_to_html.py
+++ src/test_text_to_html.py
@@ -5,6 +5,7 @@ from textnode import TextNode, TextType
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_eq(self):
+# man life is being tough, here's a quick edit to show I "did something" even though nothing's actually changed        
         return
 
 
