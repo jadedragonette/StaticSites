827c2bae065f3e8e68fa3e60b5a1a71cbb34e092
jadedragonette <jadedragonette@ymail.com>
2025-01-04 09:18:53 -0800

    super inital testing added of split_nodes_delimiter

---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,16 @@
+import unittest
+
+from textnode import *
+from htmlnode import *
+from main import *
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_eq(self):
+# man life is being tough, here's a quick edit to show I "did something" even though nothing's actually changed - and once again - hopefully a final time today       
+        return
+
+
+
+if __name__ == "__main__":
+    unittest.main()
