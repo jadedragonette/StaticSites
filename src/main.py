40e129643e95bf94a3246383bab196fc1054fa68
jadedragonette <jadedragonette@ymail.com>
2025-01-05 10:42:45 -0800

    super quick comment editing

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ def text_node_to_html_node(text_node):
         LeafNode("img", "", , {src:text_node.url, alt:text_node.text})
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+#   gotta figure this out still, brain still overwork-exhausted. Yay me
     split = old_nodes.split(delimiter)
     return
 
